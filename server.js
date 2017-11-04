2587f863359cc2ca41821bdd9f1df5ce48ad6391
Marius Kriegerowski <marius.kriegerowski@gfz-potsdam.de>
2017-11-04 12:37:38 +0100

    remove c3/d3

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ app.use(require('./controllers/api/power_meter'))
 // serves the static (html) file
 app.use(require('./controllers/static'))
 
-
-app.use(require('d3-3'))
-app.use(require('c3'))
-
 app.listen(3000, function(){
 	console.log('Server listening on', 3000)
 })
