4acf916daae5c8c7d2b799279b9203543ecad359
smart <smart@amuehle.ddns.net>
2018-03-18 20:13:40 +0100

    wip: waterlevel

--- controllers/api/power_meter.js
+++ controllers/api/power_meter.js
@@ -4,6 +4,7 @@
 // umgesetzt.
 // Use Router object to use this module as middleware
 var router = require('express').Router()
+// var dbObject = require('../../db') // connect to mongodb
 
 // using mongodb directly, here instead of mongoose
 var mongodb = require('mongodb')
@@ -15,6 +16,34 @@ MongoClient.connect(dbHost, function(err, db){
   if (err) throw err;
   dbObject = db;
 })
+
+router.get('/api/water_level/:tmin/:column', function(req, res, next) {
+
+          var tmin =  new Date(parseInt(req.params.tmin));
+
+          dbObject.collection(req.params.column).find({"time" : {$gte : tmin}}).toArray(function(err, docs){
+          if ( err ) throw err;
+          var dateArray = [];
+          var counts = [];
+       
+          for ( index in docs ){
+            var doc = docs[index];
+            var t = doc['time'];
+
+            var count = doc['count'];
+            dateArray.push({"time": t});
+            counts.push({"value" : count});
+          }
+       
+          var response = {
+            "dataset" : counts,
+            "categories" : dateArray
+          };
+
+          res.json(response);
+        });
+      }
+    )
 
 // Serves the power data at this url
 router.get('/api/power/:tmin/:column', function(req, res, next) {
