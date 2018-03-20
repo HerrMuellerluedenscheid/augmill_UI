bfd9492dc51965aa63489494cb98033d7defca6b
smart <smart@amuehle.ddns.net>
2018-03-20 12:06:26 +0100

    es6

--- ng/graph.ctrl.js
+++ ng/graph.ctrl.js
@@ -3,6 +3,7 @@ var app = angular.module('app')
 app.controller('GraphCtrl', function($scope, PowerSvc) {
 	$scope.chart = null;
 	$scope.power = null;
+	const interval = 3000.;
 	$scope.nseconds_view = 30.;
 
 	$scope.zoomGraphIn = function() {
@@ -17,45 +18,35 @@ app.controller('GraphCtrl', function($scope, PowerSvc) {
 	}
 
 	$scope.showGraph = function() {
-		$scope.column = 'power';
-		var y = ['Strom'];
-		var x = ['x'];
+		$scope.data_column = 'power';
+		var yinit = ['Strom'];
+		var xinit = ['x'];
+		var tmax = new Date();
+		var tmin = new Date() - $scope.nseconds_view*1000.;
+		
+		PowerSvc.fetch(tmin, $scope.data_column)
+			.then((response) => {$scope.power = response.data;})
+			.then(() => {
+				$scope.power.categories.forEach((element) => {xinit.push(element.time)});
+				$scope.power.dataset.forEach((element) => {yinit.push(element.value/1000.)});
+			})
 
 		$scope.chart = c3.generate({
 			bindto: '#power',
 			data:{
 				x: 'x',
-				// xFormat: 'H:%M:%S',
 				xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
-				columns: [
-					x,
-					y,
-				],
+				columns: [xinit, yinit,],
 			},
-			// point: {
-			// 	show: false
-			// },
              		axis: {
 	             	     x: {
-				// type: 'scatter',
 	             	     	type: 'timeseries',
 	             	     	tick: {
-	             	     		// format: '%H:%M:%S',
 					format: 'T%H:%M:%S'
-					// culling: {
-					// 	max: 4,
-	             	     		//         format: '%H:%M:%S',
-					// }
-			     		// rotate: 75,
-			     		// multiline: false
-	             	     		// values: ['']
 	             	     	}
 	             	     }
              		}
 		})
-		setTimeout(function(){
-		}, 500)
-		$scope.setGraphData()
 	}
 
 	$scope.setGraphData = function() {
@@ -63,34 +54,23 @@ app.controller('GraphCtrl', function($scope, PowerSvc) {
 		var y = ['Strom'];
 		var x = ['x'];
 		var tmax = new Date();
-		var tmin = new Date() - $scope.nseconds_view*1000.;
+		var tmin = new Date() - interval;
 		
-		PowerSvc.fetch(tmin=tmin, column=$scope.column).then(function(response) {
-			$scope.power = response.data;
+		PowerSvc.fetch(tmin, $scope.data_column)
+			.then((response) => {$scope.power = response.data})
+
+		$scope.power.categories.forEach((element) => {x.push(element.time)});
+		$scope.power.dataset.forEach((element) => {y.push(element.value/1000.)});
+		$scope.chart.axis.max({y: 5.});
+		$scope.chart.axis.min({y: 2.});
+
+		$scope.lastPower = y[y.length-1];
+		$scope.chart.flow({
+			columns: [x, y],
+			duration: 2000.,
 		})
-
-		if ($scope.power !== null) {
-			$scope.power.categories.forEach(function(element){
-				x.push(element.time);
-			})
-			$scope.power.dataset.forEach(function(element){
-				y.push(element.value/1000.);
-			})
-			$scope.chart.axis.max({y: 5.});//, x: new Date()});
-			$scope.chart.axis.min({y: 2.}); //, x: tmin});
-
-			// $scope.chart.axis.tick({x: [x[0], x[x.length-1]]})
-			$scope.lastPower = y[y.length-1];
-			$scope.chart.flow({
-				columns: [
-					x,
-					y
-				],
-				duration: 2000.,
-			})
-		}
 
 	}
 
-	setInterval($scope.setGraphData, 3000);
+	setInterval($scope.setGraphData, interval);
 })
