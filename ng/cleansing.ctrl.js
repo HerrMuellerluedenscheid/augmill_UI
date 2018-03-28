742fc1769353253f6219c4c7f0d0fba34c6a99cd
HerrMuellerluedenscheid <marius.kriegerowski@gfz-potsdam.de>
2018-03-28 14:22:45 +0200

    tmp disabel button

--- ng/cleansing.ctrl.js
+++ ng/cleansing.ctrl.js
@@ -3,21 +3,35 @@ var app = angular.module('app')
 // declare a controller, with $scope and dependency inject the PostsSvc:
 app.controller('CleansingCtrl', ['$scope', '$http', '$mdDialog', function($scope, $http, $mdDialog) {
 	console.log($mdDialog)
-	  $scope.reinigungsKlappe = function(ev) {
-	    var confirm = $mdDialog.confirm()
-	          .title('Rechen Reinigen?')
-	          .textContent('Sicher?')
-	          .ariaLabel('Lucky day')
-	          .targetEvent(ev)
-	          .ok('OK')
-	          .cancel('Abbrechen');
 
-	    $mdDialog.show(confirm).then(function() {
-	      $scope.status = 'start cleaning';
-	      return $http.get('/api/clean');
-	    }, function() {
-	      $scope.status = 'You decided to keep your debt.';
-	    });
-	  }
+	function disableButton (duration){
+		$scope.buttonDisabled = true;
+		$timeout(function() { 
+			$scope.buttonDisabled = false;
+		}, duration);
+	}
+
+	$scope.disableButton = function(duration) {
+		$scope.buttonDisabled = true;
+		setTimeout(function() {$scope.buttonDisabled = false;}, duration)
+	}
+
+	$scope.reinigungsKlappe = function(ev) {
+		var confirm = $mdDialog.confirm()
+		.title('Rechen Reinigen')
+		.textContent('Sicher?')
+		.ariaLabel('Lucky day')
+		.targetEvent(ev)
+		.ok('OK')
+		.cancel('Abbrechen');
+
+		$mdDialog.show(confirm).then(function() {
+			$scope.status = 'start cleaning';
+			disableButton(3000.);
+			// return $http.get('/api/clean');
+		}, function() {
+			$scope.status = 'You decided to keep your debt.';
+		});
+	}
 }]
 )
