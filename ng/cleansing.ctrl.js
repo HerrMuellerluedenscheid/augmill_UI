31645a28552620f6f9072aed30a16170805cf208
smart <smart@amuehle.ddns.net>
2018-03-29 05:00:41 +0200

    fix disable/enable button

--- ng/cleansing.ctrl.js
+++ ng/cleansing.ctrl.js
@@ -1,19 +1,17 @@
 var app = angular.module('app')
 
 // declare a controller, with $scope and dependency inject the PostsSvc:
-app.controller('CleansingCtrl', ['$scope', '$http', '$mdDialog', function($scope, $http, $mdDialog) {
-	console.log($mdDialog)
+app.controller('CleansingCtrl', ['$scope', '$http', '$mdDialog', '$timeout', function($scope, $http, $mdDialog, $timeout) {
 
-	function disableButton (duration){
-		$scope.buttonDisabled = true;
+	$scope.btnDisabled = false;
+	function disableButton(duration){
+		console.log('disable');
+		console.log($scope.btnDisabled);
+		$scope.btnDisabled = true;
+		console.log($scope.btnDisabled);
 		$timeout(function() { 
-			$scope.buttonDisabled = false;
+			$scope.btnDisabled = false;
 		}, duration);
-	}
-
-	$scope.disableButton = function(duration) {
-		$scope.buttonDisabled = true;
-		setTimeout(function() {$scope.buttonDisabled = false;}, duration)
 	}
 
 	$scope.reinigungsKlappe = function(ev) {
@@ -26,8 +24,7 @@ app.controller('CleansingCtrl', ['$scope', '$http', '$mdDialog', function($scope
 		.cancel('Abbrechen');
 
 		$mdDialog.show(confirm).then(function() {
-			$scope.status = 'start cleaning';
-			disableButton(3000.);
+			disableButton(10000.);
 			// return $http.get('/api/clean');
 		}, function() {
 			$scope.status = 'You decided to keep your debt.';
